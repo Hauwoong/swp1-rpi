b7542fb31e22bdc9c3c0b64f27c3c57d165993c4
Junwoong Park <pjw060311@naver.com>
2026-06-11 00:15:37 +0900

    Update graph.py and template.py paths; add .gitignore

--- graph.py
+++ graph.py
@@ -1,14 +1,14 @@
-from cgi import parse_qs
+from urllib.parse import parse_qs
 from template import html
 import matplotlib.pyplot as plt
 
 def application(environ, start_response):
-    if environ['PATH_INFO'] == '/graph.png':
+    if environ['PATH_INFO'] == '/img/graph.png':
         try:
-            with open('graph.png', 'rb') as f:
+            with open('./img/graph.png', 'rb') as f:
                 response_body = f.read()
         except:
-            response_body = ''
+            response_body = b''
         start_response('200 OK', [
             ('Content-Type', 'image/png'),
             ('Content-Length', str(len(response_body)))
@@ -20,13 +20,13 @@ def application(environ, start_response):
         b = d.get('b', [''])[0]
         c = d.get('c', [''])[0]
         if '' not in [a, b, c]:
-            a, b, c = int(a), int(b), int(c)
+            a, b, c = int(float(a)), int(float(b)), int(float(c))
             x = [n / 10.0 for n in range(-40, 41)]
             y = [a * n ** 2 + b * n + c for n in x]
             fig = plt.figure()
             graph = plt.plot(x, y)
             plt.grid()
-            fig.savefig('graph.png')
+            fig.savefig('./img/graph.png')
         response_body = html
         start_response('200 OK', [
             ('Content-Type', 'text/html'),
